c70ced452ea0c2796fb0cbb33e6fe43f358f85c2
Wladimir Mutel <mwg@mwg.dp.ua>
2024-01-18 21:58:11 +0200

    more reorders

--- upgrade-lubuntu3264.sh
+++ upgrade-lubuntu3264.sh
@@ -19,12 +19,14 @@ dpkg -l dpkg | grep -w i.86 && {
  apt update
 }
 
-dpkg -l systemd | grep -w i.86 && {
- apt install $(awk '$2=="install"{print $1}' $pfl | egrep -i 'systemd|udev' | sed 's/:i386//')
-}
-
 apt install perl-base:amd64
 apt -f install
+
+dpkg -l systemd | grep -w i.86 && {
+ apt install $(awk '$2=="install"{print $1}' $pfl | egrep -i 'systemd|udev' | sed 's/:i386//')
+ apt -f install
+}
+
 apt install base-files:amd64 base-passwd:amd64 bash:amd64 bsdutils:amd64 coreutils:amd64 debianutils:amd64 diffutils:amd64 e2fsprogs:amd64 fdisk:amd64 findutils:amd64 grep:amd64 gzip:amd64 hostname:amd64 init:amd64 libc-bin:amd64 login:amd64 mount:amd64 ncurses-bin:amd64 sed:amd64 sudo:amd64 sysvinit-utils:amd64 tar:amd64 util-linux:amd64
 dpkg -l dash | grep -w i.86 &&
 { dpkg-divert --remove /bin/sh
