adb4a1e958a8394bb37006441c91739d30402f43
Wladimir Mutel <mwg@mwg.dp.ua>
2024-01-18 20:55:51 +0200

    improvements

--- upgrade-lubuntu3264.sh
+++ upgrade-lubuntu3264.sh
@@ -1,7 +1,7 @@
 # wget https://raw.githubusercontent.com/muwlgr/scripts/main/upgrade-lubuntu3264.sh
 # sudo bash upgrade-lubuntu3264.sh
 
-uname -m | grep 'i.86' && 
+uname -m | grep 'i.86' &&
 { dpkg --add-architecture amd64
   apt update
   apt install linux-image-generic:amd64
@@ -10,14 +10,18 @@ uname -m | grep 'i.86' &&
 
 apt install apt:amd64 apt-utils:amd64 dpkg:amd64
 apt update
-dpkg -l dash | grep -w i.86 && 
+apt install perl-base:amd64 || apt -f install
+apt install base-files:amd64 base-passwd:amd64 bash:amd64 bsdutils:amd64 coreutils:amd64 debianutils:amd64 diffutils:amd64 e2fsprogs:amd64 fdisk:amd64 findutils:amd64 grep:amd64 gzip:amd64 hostname:amd64 init:amd64 libc-bin:amd64 login:amd64 mount:amd64 ncurses-bin:amd64 sed:amd64 sudo:amd64 sysvinit-utils:amd64 tar:amd64 util-linux:amd64
+dpkg -l dash | grep -w i.86 &&
 { dpkg-divert --remove /bin/sh
   dpkg-divert --remove /usr/share/man/man1/sh.1.gz
   apt install dash:amd64
 }
-apt install perl-base:amd64
-apt install base-files:amd64 base-passwd:amd64 bash:amd64 bsdutils:amd64 coreutils:amd64 debianutils:amd64 diffutils:amd64 e2fsprogs:amd64 fdisk:amd64 findutils:amd64 grep:amd64 gzip:amd64 hostname:amd64 init:amd64 libc-bin:amd64 login:amd64 mount:amd64 ncurses-bin:amd64 sed:amd64 sysvinit-utils:amd64 tar:amd64 util-linux:amd64 
-apt install $(dpkg -l | awk '$1=="ii"&&$2~/:i386/{print $2}' | sed 's/:i386/:amd64/')
+pkgs() { # $1 - architecture
+ dpkg -l | awk '$4=="'$1'"{print $2}' | awk -F: '{print $1}' | sort -u
+}
+apt install $(comm -23 <(pkgs i386) <(pkgs amd64))
+#apt install $(dpkg -l | awk '$1=="ii"&&$2~/:i386/{print $2}' | sed 's/:i386/:amd64/')
 apt remove $(dpkg -l | awk '$1=="ii"&&$2~/:i386/{print $2}')
 apt autoremove
 
