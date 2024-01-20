0d5c3af2bdcf14a7fdcd5b774bce0549458f6623
Wladimir Mutel <mwg@mwg.dp.ua>
2024-01-20 11:51:40 +0200

    save&restore auto marks, more acpid changes

--- upgrade-lubuntu3264.sh
+++ upgrade-lubuntu3264.sh
@@ -4,9 +4,11 @@
 # sudo bash upgrade-lubuntu3264.sh
 
 pfl=pkgs1.tmp
+aapkg=apt-auto.tmp
 
 uname -m | grep i.86 && { # while we are under 32-bit kernel
  dpkg --get-selections > $pfl # save installed package list
+ apt-mark showauto | sed 's/:.*//' | sort -u > $aapkg # save auto marks
  dpkg --add-architecture amd64
  apt update
  apt install linux-image-generic:amd64 thermald:i386 # this will pull in iucode-tool:amd64, not runnable under i386, but this is not fatal
@@ -38,12 +40,15 @@ dpkg -l dash | grep -w i.86 && { # remove diverts before upgrading dash, https:/
 
 dpkg -l acpid | grep -w i.86 && { # acpid upgrade bug at https://bugs.launchpad.net/ubuntu/+source/acpid/+bug/1760391
  apt install acpid:amd64
- systemctl restart acpid.socket
- systemctl restart acpid
- apt -f install
+ systemctl --failed | grep acpid && {
+  systemctl restart acpid.socket
+  systemctl restart acpid 
+ }
+ dpkg -l acpid | grep 'iF *acpid ' && apt -f install
 }
 
 apt install $(awk '$2=="install"{print $1}' $pfl | grep -v :i386) # upgrade the rest
+apt-mark auto $(cat $aapkg) # restore auto marks for apt autoremove
 apt autoremove
 
 pkgs() { # $1 - architecture
