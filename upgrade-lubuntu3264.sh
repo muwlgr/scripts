9cc6ad84bc3cbb25ca23f9b53f2e441d9fc994d8
Wladimir Mutel <mwg@mwg.dp.ua>
2024-01-20 08:20:42 +0200

    more reorders and autoremoves

--- upgrade-lubuntu3264.sh
+++ upgrade-lubuntu3264.sh
@@ -20,19 +20,22 @@ dpkg -l dpkg | grep -w i.86 && { # upgrade apt and dpkg
 
 apt install perl-base:amd64 # first critical package to upgrade separately
 apt -f install
+apt autoremove
 
 dpkg -l systemd | grep -w i.86 && { # need to complete systemd upgrade before udev could be properly restarted
  apt install $(awk '$2=="install"&&$1~/systemd/{print $1}' $pfl | sed 's/:i386//')
 }
 
+
+#critical packages better to upgrade in advance
+apt install base-files:amd64 base-passwd:amd64 bash:amd64 bsdutils:amd64 coreutils:amd64 debianutils:amd64 diffutils:amd64 e2fsprogs:amd64 fdisk:amd64 findutils:amd64 grep:amd64 gzip:amd64 hostname:amd64 init:amd64 libc-bin:amd64 login:amd64 mount:amd64 ncurses-bin:amd64 sed:amd64 sudo:amd64 sysvinit-utils:amd64 tar:amd64 util-linux:amd64
+apt autoremove
+
 dpkg -l dash | grep -w i.86 && { # remove diverts before upgrading dash, https://askubuntu.com/questions/81824
  dpkg-divert --remove /bin/sh
  dpkg-divert --remove /usr/share/man/man1/sh.1.gz
+ apt install dash:amd64
 }
-
-#critical packages better to upgrade in advance
-apt install base-files:amd64 base-passwd:amd64 bash:amd64 bsdutils:amd64 coreutils:amd64 dash:amd64 debianutils:amd64 diffutils:amd64 e2fsprogs:amd64 fdisk:amd64 findutils:amd64 grep:amd64 gzip:amd64 hostname:amd64 init:amd64 libc-bin:amd64 login:amd64 mount:amd64 ncurses-bin:amd64 sed:amd64 sudo:amd64 sysvinit-utils:amd64 tar:amd64 util-linux:amd64
-apt autoremove
 
 apt install $(awk '$2=="install"{print $1}' $pfl | grep -v :i386) # upgrade the rest
 apt autoremove
