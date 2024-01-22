e5357cb6532366df090b709146d7ca7de31cf23d
Wladimir Mutel <mwg@mwg.dp.ua>
2024-01-22 21:51:52 +0200

    simplify install of lubuntu on debian12/bookworm

--- build-lubuntu-on-bookworm.sh
+++ build-lubuntu-on-bookworm.sh
@@ -2,8 +2,10 @@
 apt install debhelper germinate dpkg-dev icon-naming-utils intltool gir1.2-rsvg-2.0 scour gpg 
 
 getpgpkey(){ # $1 - fingerprint, $2 - keyserver, $3 - output file
- gpg --recv-keys --keyserver $2 $1
- gpg --output $3 --armour --export $1
+ [ -f $3 ] || {
+  gpg --recv-keys --keyserver $2 $1
+  gpg --output $3 --armour --export $1
+ }
 }
 
 # get Ubuntu keys and trust them
@@ -21,13 +23,19 @@ set -- deb-src http://ua.archive.ubuntu.com/ubuntu bionic
 
 apt update
 cd
+
+#clean
+for i in lubuntu-meta-* 
+do [ -d $i ] && rm -rv $i
+done
+
 # get bionic package sources 
-apt source lubuntu-meta lubuntu-artwork lubuntu-default-settings
+apt source lubuntu-meta 
 
 # patch them to remove unneeded dependencies
 ( cd lubuntu-meta-*
-  sedprog='s/fcitx-config-gtk2/fcitx-config-gtk/;s/firefox$/firefox-esr/;s/leafpad/l3afpad/
-           /abiword|gnumeric|gdebi|fwupdate|kerneloops|whoopsie|apport|indicator-|language-select|usb-creat|plymouth|gtk2-perl|ubuntu-(driver|release|font)|update-notifier|ubufox/d'
+  sedprog='s/firefox$/firefox-esr/;s/leafpad/l3afpad/
+           /abiword|gnumeric|gdebi|fcitx|fwupdate|kerneloops|whoopsie|apport|indicator-|language-select|usb-creat|plymouth|gtk2-perl|ubuntu-(driver|release)|update-notifier|ubufox/d'
   { egrep -i '(seeds|architectures):' update.cfg | tr -d ':' | while read a b 
     do echo for $a in $b ';' do
     done
@@ -39,27 +47,12 @@ apt source lubuntu-meta lubuntu-artwork lubuntu-default-settings
      diff $fn <(sed -r "$sedprog" $fn) || sed -r -i "$sedprog" $fn
   done
 )
-( cd lubuntu-default-settings-*
-  sed -i '/ubuntu-font/d' debian/control
-)
 
 #build bionic pkgs
-for i in lubuntu-meta-* lubuntu-artwork-* lubuntu-default-settings-*
+for i in lubuntu-meta-* 
 do ( cd $i
-     dpkg-buildpackage -b )
+     dpkg-buildpackage -b --no-sign )
 done
 
-# connect to jammy sources
-sed 's/bionic/jammy/' /etc/apt/sources.list.d/bionic.list > /etc/apt/sources.list.d/jammy.list
-
-apt update
-# get jammy package sources 
-apt source ubuntu-themes policykit-desktop-privileges
-
-#build jammy pkgs
-for i in ubuntu-themes-* policykit-desktop-privileges-*
-do ( cd $i
-     dpkg-buildpackage -b )
-done
-
-tar czf /tmp/lubuntu-bookworm.tgz lubuntu-desktop_*_*.deb lubuntu-gtk-core_*_*.deb lubuntu-gtk-desktop_*_*.deb lubuntu-artwork_*_*.deb lubuntu-artwork-18-04_*_*.deb lubuntu-lxpanel-icons_*_*.deb lubuntu-default-session_*_*.deb lubuntu-default-settings_*_*.deb lubuntu-icon-theme_*_*.deb ubuntu-mono_*_*.deb
+tar czvf /tmp/lubuntu-bookworm.tgz lubuntu-desktop_*_*.deb lubuntu-gtk-core_*_*.deb lubuntu-gtk-desktop_*_*.deb 
+ls -lh /tmp/lubuntu-bookworm.tgz
--- install-lubuntu-on-bookworm.sh
+++ install-lubuntu-on-bookworm.sh
@@ -1,10 +1,49 @@
+ldmdeps='desktop-base fonts-quicksand gnome-accessibility-themes gnome-themes-extra gnome-themes-extra-data gtk2-engines gtk2-engines-pixbuf libayatana-ido3-0.4-0 libayatana-indicator3-7 libevdev2 libinput-bin libinput10 liblightdm-gobject-1-0 libmtdev1 libplymouth5 libwacom-common libwacom9 libxatracker2 libxcb-util1 libxcvt0 libxfont2 libxklavier16 libxvmc1 lightdm-gtk-greeter plymouth plymouth-label  x11-xkb-utils xcvt xfonts-base xserver-common xserver-xorg xserver-xorg-core xserver-xorg-input-all xserver-xorg-input-libinput xserver-xorg-input-wacom xserver-xorg-legacy xserver-xorg-video-all xserver-xorg-video-amdgpu xserver-xorg-video-ati xserver-xorg-video-fbdev xserver-xorg-video-intel xserver-xorg-video-nouveau xserver-xorg-video-qxl xserver-xorg-video-radeon xserver-xorg-video-vesa xserver-xorg-video-vmware'
+
+apt install lightdm $ldmdeps
+apt-mark auto $ldmdeps
+
+getpgpkey(){ # $1 - fingerprint, $2 - keyserver, $3 - output file
+ [ -f $3 ] || {
+  gpg --recv-keys --keyserver $2 $1
+  gpg --output $3 --armour --export $1
+ }
+}
+
+ks=keyserver.ubuntu.com
+
+# get Ubuntu keys and trust them
+( cd /etc/apt/trusted.gpg.d/
+  getpgpkey 3B4FE6ACC0B21F32 $ks bionic.asc
+  getpgpkey 871920D1991BC93C $ks jammy.asc
+)
+
+genubuntulist() { # $1 - deb or deb-src, $2 - mirror URL, $3 - suite name
+ { for i in $3 $3-updates $3-security
+   do echo $1 $2 $i main restricted universe multiverse
+   done
+ } > /etc/apt/sources.list.d/$3.list
+ apt update
+}
+
+ubmir=http://ua.archive.ubuntu.com/ubuntu
+
+genubuntulist deb $ubmir jammy
+apt install policykit-desktop-privileges ubuntu-mono
+rm /etc/apt/sources.list.d/jammy.list
+
+genubuntulist deb $ubmir bionic
+apt install lubuntu-icon-theme lubuntu-default-settings lubuntu-default-session
+rm /etc/apt/sources.list.d/bionic.list
+apt update
+
 # small workaround for Ubuntu's libnewt
 #mkdir /etc/newt
 #touch /etc/newt/palette.original
 #update-alternatives --install /etc/newt/palette newt-palette /etc/newt/palette.original 20
 
 # install locally built .debs
-dpkg -i lubuntu-desktop_*_*.deb lubuntu-gtk-core_*_*.deb lubuntu-gtk-desktop_*_*.deb lubuntu-artwork_*_*.deb lubuntu-artwork-18-04_*_*.deb lubuntu-lxpanel-icons_*_*.deb lubuntu-default-session_*_*.deb lubuntu-default-settings_*_*.deb lubuntu-icon-theme_*_*.deb ubuntu-mono_*_*.deb
+dpkg -i lubuntu-desktop_*_*.deb lubuntu-gtk-core_*_*.deb lubuntu-gtk-desktop_*_*.deb 
 apt -f install
 
 sed -i '/#greeter-hide-users=false/s/^#//' /etc/lightdm/lightdm.conf
