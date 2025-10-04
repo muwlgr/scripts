a29dda265607442b2b163b45712f78dcbf628b4d
Wladimir Mutel <mwg@mwg.dp.ua>
2025-10-04 09:33:41 +0300

    Added Mysql 8.* upgrade from Oracle sources

--- upgrade-jammy-to-bookworm.sh
+++ upgrade-jammy-to-bookworm.sh
@@ -1,3 +1,4 @@
+set -x -e
 
 gen_debian_list() { # $1 - deb or deb-src, $2 - mirror URL, $3 - suite name , $4 - sections , $5 - sec.upd. url/path
  { for i in $3 $3-updates 
@@ -16,27 +17,37 @@ drop_sources() { # $1 - distro name
  find /etc/apt/sources* -type f -iname '*.list' | xargs grep -l '^ *deb .* '$1 | while read l
  do sed -r -i '/^ *deb/s/^/#/' $l # disconnect from specified repos
  done
+ grep -lw $1 /etc/apt/sources*/*.sources | while read l 
+ do mv $l $l.disabled
+ done
 }
 
-debian_quick_upgrade(){ # $1 - old distro, $2 - new distro , $3 - arch , $4 - debmir
- drop_sources $1
- gen_debian_list_$2 deb $4 $2
- apt install apt dpkg
- apt update
- type lspci || apt install pciutils
- lspci | grep -i realtek && apt install firmware-realtek
- lspci | grep -i atheros && apt install firmware-atheros
- lspci | egrep -i 'vga.*(intel|nvidia)' && apt install firmware-misc-nonfree
- dpkg -l | grep -i linux-header && apt install linux-headers-$3
- apt install $(dpkg -l | egrep 'ii *(openssh-server|network-manager|initramfs-tools|libc6|locales|systemd|resolv|login|passwd|util-linux|sudo)' | awk '{print $2}') linux-image-$3 # upgrade important pkgs
- [ -f /lib/systemd/systemd-resolved ] || apt install systemd-resolved
- [ -f /lib/systemd/systemd-timesyncd ] || apt install systemd-timesyncd
- apt install $(dpkg -l | egrep 'ii *(base-files|lsb-release)' | awk '{print $2}') # bump lsb_release version
- apt autoremove
-# reboot
+newv(){ # stdin: list of packages, 1 per line , $1 - distro name
+ ( set +x 
+   while read pp
+   do ubuv=$(dpkg -l $pp | awk '$2~/'$pp'(:[^ ])?/ {print $3}')
+      debv=$(apt-cache showpkg $pp | egrep $1'.*Packages' | grep -v '^ ' | sed 's/ (.*//' | sort -V | tail -n1)
+      [ "$debv" ] && [ "$debv" != "$ubuv" ] && echo $pp=$debv
+   done )
 }
 
-debmir=http://debian.volia.net/debian
+iigrep(){ # $1 - egrep regexp
+ dpkg -l | egrep -i '^ii +('"$1"')' | awk '{gsub(/:[^:]*/,"",$2);print $2}'
+}
+
+reinstall_so(){ # $1 - $solist, $2 - package list with versions pkg=ver
+ [ "$2" ] || return 0
+ apt install $2 || : # this may fail due to improperly removed .so libs
+ missing_so="$( cd /var/lib/dpkg/info
+                grep -h bin/ *list | xargs file | awk -F: '/ELF.*dyn.*link/{print $1}' | sort -u | # find ELF dynamic linkec executables
+                xargs ldd | grep 'not found' | awk '{print $1}' | sort -u )" # find what .so libs they are lacking
+
+ [ "$missing_so" ] || return 0
+ pkgl2=$( dpkg -l | fgrep -f <(fgrep -f <(echo "$missing_so") $1 | sed 's/\.list:.*//;s/.*\///;s/:.*//;s/t64//' | sort -u) | # find what packages they should be in
+          awk '{print $2}' | awk -F: '{print $1}' | sort -u)
+ [ "$pkgl2" ] || return 0
+ apt reinstall $pkgl2 
+}
 
 arch='x'
 case $(uname -m) in
@@ -44,19 +55,62 @@ case $(uname -m) in
  x86_64) arch=amd64 ;;
 esac
 
-pfl=pkg-bionic.tmp
-aapkg=aapkg-bionic.tmp
+debmir=$(find /etc/apt/sources.list* -type f | xargs awk '$1=="deb" && $3~/'$olddistro'/{print $2}' | sort -u | fgrep -v ubuntu.com | sed 's/ubuntu/debian/g'
+         # or add echo $your own mirror and comment out the line above, else you would get errors everywhere
+         )
+set -- $debmir
+[ $# -eq 1 ] || debmir=http://debian.volia.net/debian
+olddistro=jammy
+distro=bookworm # fill missing pkgs from Bookworm
+pfl=$(pwd)/pkg-$olddistro.tmp
+aapkg=$(pwd)/aapkg-$olddistro.tmp
+flist=$(pwd)/dpkg-$olddistro.tmp
+solist=$(pwd)/solibs-$olddistro.tmp
 
 main(){ # called recursively for upgrade without reboot
  type lsb_release || apt install lsb-release
  case $(lsb_release -sc) in
-  jammy) 
-   dpkg --get-selections > $pfl
-   apt-mark showauto | sed 's/:.*//' | sort -u > $aapkg
+  $olddistro)
+   # save package lists and states
+   [ -f $pfl ] || dpkg --get-selections > $pfl
+   [ -f $aapkg ] || apt-mark showauto | sed 's/:.*//' | sort -u > $aapkg
+   [ -s $flist ] || dpkg -l > $flist
 
-   apt remove $(dpkg -l | egrep -i 'appstream|fcitx|zjs|gnome-|abiword|gnumeric|manpages|mailcap|mime-support|usb-creator|fwupd|transmission|audacious|xf[bc]|system-config|indicator|firefox|gdebi|language-pack|linux-firmware|gstreamer-plugins|light|pulse|chromaprint|freerdp|pidgin|purple|gudev|[ b]gconf|nmap|lib(input|wacom|qt)' | awk '{print $2}' ) pinentry-curses 
+   # revert some ubuntu customizations
+   dpkg -l | egrep -i '^ii +language-pack' && ls /var/lib/locales/supported.d/* && sort -u /var/lib/locales/supported.d/* | grep '^[A-Za-z]' > /tmp/locale.gen
+   find /etc/profile.d -type f | xargs grep -l '^eval .*locale-check' | while read l
+   do sed -r -i '/^ *eval .*locale-check/s/^/#/' $l
+   done
+   ( cd /etc
+     for i in passwd group shadow gshadow
+     do grep '^gdm:' $i && ! grep '^Debian-gdm:' $i && echo Debian-$(grep '^gdm:' $i) >> $i || : # debian gdm3 hack
+     done )
+
+   dpkg -l | egrep -i '^ii +snapd .*build' && snap remove firefox snapd-desktop-integration # this damned crap prevents snapd from being purged
+   apt purge $(iigrep '(ubuntu-(advantage-tools|pro-client)|(apparmor|plasma-welcome|libplymouth5).*ubuntu.*) ') 
+   apt remove $(iigrep '(.*(appstream|app-config-icons|printer-driver|xrender)).* .*build|(.*(baloowidget|pg-agent|libreoffice|uno-libs|flac|glib2|fontconfig|activi|kwayland)).* .*ubuntu|.*(ge-pa|xorg)')
+   [ -s /tmp/locale.gen ] && {
+    comm -23 /tmp/locale.gen <(grep '^[A-Za-z]' /etc/locale.gen | sort -u) >> /etc/locale.gen
+    tail /etc/locale.gen
+   }
    apt autoremove
-   apt install $(dpkg -l | egrep '^ii (.*[ou]code|locales)' | awk '{print $2}') #keep amd64-microcode intel-microcode iucode-tool
+
+   dpkg -l | egrep '^ii +network-manager' || { # prepare to drop netplan
+    dpkg -l | egrep -i '^ii +netplan' && {
+     ls -l /etc/systemd/network/* || cp -av /run/systemd/network/* /etc/systemd/network/ || :
+     systemctl is-enabled systemd-networkd || systemctl enable systemd-networkd 
+    }
+   }
+
+   dpkg -l | grep '^ii *debian-archive-keyring .*2023' || {
+    set -- $(wget -O - https://packages.debian.org/$distro/all/debian-archive-keyring/download | grep -io 'http.*all\.deb' | sort -u) # get package name from its DL page
+    wget $1
+    dpkg -i $(basename $1)
+   }
+
+   apt remove $(iigrep 'appstream|fcitx|zjs|gnome-|abiword|gnumeric|manpages|mailcap|mime-support|usb-creator|fwupd|transmission|audacious|xf[bc]|system-config|indicator|firefox|gdebi|language-pack|linux-firmware|gstreamer-plugins|light|pulse|chromaprint|freerdp|pidgin|purple|gudev|[ b]gconf|nmap|lib(input|wacom|qt|ry-cu)' ) 
+   apt autoremove
+   apt install $(iigrep '.*[ou]code|locales') #keep amd64-microcode intel-microcode iucode-tool
    dpkg-reconfigure locales
    apt autoremove
    find /etc/profile.d -type f | xargs grep -l '^eval .*locale-check' | while read l
@@ -64,35 +118,61 @@ main(){ # called recursively for upgrade without reboot
    done
    apt purge $(dpkg -l | egrep -i ' (ubuntu-advantage-tools|snapd) ' | awk '{print $2}')
 
-   distro=bookworm
-   dpkg -l | grep '^ii *debian-archive-keyring ' || {
-    set -- $(wget -O - https://packages.debian.org/$distro/all/debian-archive-keyring/download | grep -io 'http.*all\.deb' | sort -u) # get package name from its DL page
-    wget $1
-    dpkg -i $(basename $1)
-   }
-
-   drop_sources jammy
+   drop_sources $olddistro
    gen_debian_list_$distro deb $debmir $distro # some downgrades before quick upgrade
-   dpkg -l | egrep -i '(grub|shim).*-signed .*ubuntu.*' && { # downgrade grub&shim signed pkgs
-    apt install grub-efi-amd64-signed=1+2.06+13+deb12u1 shim-signed=1.39+15.7-1 shim-signed-common=1.39+15.7-1 ||
-    apt -f install
-   }
-   ubuv='12.3.0-1ubuntu1~22.04'
-   debv='12.2.0-14'
-   apt install $(dpkg -l | awk '$3=="'$ubuv'"{print $2"='$debv'"}') || apt -f install # downgrade gcc from Jammy to BookWorm
-   dpkg -l | grep systemd.*249 && apt install systemd-resolved
+   set -- $(iigrep mysql-server)
+   [ $# -gt 0 ] && wget https://repo.mysql.com//mysql-apt-config_0.8.33-1_all.deb
    
-   debian_quick_upgrade jammy $distro $arch $debmir 
-   apt remove $(dpkg -l | egrep -i 'ubuntu-|n(et)?plan' | awk '{print $2}' )
+   grep '^ii *eatmydata' $flist && apt install eatmydata
+   [ -f $solist ] || ( cd /var/lib/dpkg/info 
+                       egrep '\.so(\.|$)' *.list ) > $solist
+   apt install $(iigrep 'apt|dpkg|gzip' | newv $distro)
+   apt install $(iigrep 'shim-signed .*ubuntu' | newv $distro) || apt -f install
+   apt autoremove
+   apt download libapt-pkg6.0
+   apt install $(iigrep 'libapt.*t64' | sed 's/t64//' | newv $distro) # here we may lose libapt-pkg*.so, but reinstall_so would not help as it uses apt inside
+   apt help || { dpkg -i libapt-pkg*deb # dirty little fix
+                 rm -v libapt-pkg*deb 
+               }
+   apt update
+   reinstall_so $solist "$(iigrep '.*' | grep -v base-files | newv $distro)"
+   reinstall_so $solist "$(iigrep '.*' | grep -v base-files | newv $distro)" # this may need to be restarted to upgrade remaining pkgs
+   reinstall_so $solist "$(iigrep '.*t64.* ' | sed 's/t64//' | newv $distro)" # downgrade Trixie t64 libs to Bookworm's non-t64
+   apt install $(iigrep 'base-files' | newv $distro) # finally install base-files
+   [ -f mysql-apt-config*.deb ] && { apt install gnupg
+                                     dpkg -i mysql-apt-config*.deb
+                                     apt update
+                                   }
+   apt remove $(iigrep '(n(et)?plan.*ubuntu|.*(ubuntu|pd-si|op-pr|ub-gf)|(libaio|secureboot).* .*build)' | grep -v yaru )
    apt autoremove
    main
   ;;
-  bookworm)
-   apt install $(dpkg -l | egrep 'ii *(acpid)' | awk '{print $2}')
-   apt upgrade || apt -f install
+  $distro)
+   apt install $(for i in $(comm -23 <(grep '^ii ' $flist | egrep -v 'kf5kdelibs|libldb|libsmb|linux-gnu|t64' | awk '{print $2}' | awk -F: '{print $1}' |
+                                       sort -u) <(dpkg -l | grep '^ii ' | awk '{print $2}' | awk -F: '{print $1}' | sort -u)) 
+                 do apt-cache showpkg $i | egrep -qi $distro && echo $i 
+                 done) # reinstall all pkgs removed from Ubuntu which have Debian equivalent
+   apt install $( pci=$(mktemp)
+                  lspci > $pci
+                  grep -iq realtek $pci && echo firmware-realtek 
+                  grep -iq atheros $pci && echo firmware-atheros 
+                  grep -iq 'intel.*wifi' $pci && echo firmware-iwlwifi 
+                  egrep -iq 'vga.*(intel|nvidia)' $pci && echo firmware-misc-nonfree 
+                  egrep -iq 'intel.*sound'  $pci && echo firmware-sof-signed 
+                  rm $pci
+                  grep -iq '^ii *linux-header' $flist && echo linux-headers-$arch 
+                  grep -iq '^ii *firefox .*snap.*ubuntu.*' $flist && echo firefox-esr 
+                  echo linux-image-$arch 
+                  grep -q '^ii *gnome-session-bin ' $flist && echo gnome-session )
+   apt remove $( for i in $(dpkg -l | grep '^ii *.*'$(uname -r) | awk '{print $2}') 
+                 do apt-cache showpkg $i | egrep -iq $distro || echo $i 
+                 done ) # and ubuntu's running kernel
+   re='bzip2|ptitu|exfat|exim|2-ma|me-(br|sh|so)|cp-co|on-th|d2-pl|ev-cr|or-0-pl|ls-bi|td-pk|io-ut|n3-(ke|ren|sm|uri|web)|md-co|ib-me|lynx|perl-tk|sy-rs|pcscd|realmd|rtmp|wayland|foomatic|paps|pinentry|gstream.*(ugly|libav|qt5)| (tk[0-9.]+|tix) '
+   apt remove $(comm -23 <(dpkg -l | egrep -i "$re" | awk '$1=="ii"{print$2}' | awk -F: '{print $1}' |
+                           sort -u) <(egrep -i "$re" $flist | awk '$1=="ii"{print$2}' | awk -F: '{print $1}' |
+                                      sort -u)) # clean after Debian
    apt autoremove
    apt dist-upgrade 
-   apt autoremove
   ;;
  esac
 }
