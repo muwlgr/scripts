4dbad4cd3f811d539a7caba1a855d3a92393c7a3
Wladimir Mutel <mwg@mwg.dp.ua>
2026-02-11 20:08:52 +0200

    copy improvements from older version of runme

--- debootstrap/runme.sh
+++ debootstrap/runme.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
-set -x
-[ $( id -u ) = 0 ] || exit 1
-root=$( dirname $0 | sed 's/root.*//' )
-cd $root &&
+set -x -e
+[ $( id -u ) = 0 ] # fail for non-root
+root=$(dirname $(dirname $(realpath $0)))
+cd $root
 for i in dev dev/pts proc run sys tmp var/cache/apt/archives
-do mount --bind /$i ./$i || exit 1
-done &&
+do [ -d /$i ] && mount --bind /$i ./$i
+done
 grep ^efivar /proc/mounts | 
 while read a b c 
 do mount --bind $b .$b 
-done
-chroot .
-while [ $(fgrep -c $root /proc/mounts) -gt 0 ]
-do fgrep $root /proc/mounts | while read h i j
-   do umount $i
+done || :
+chroot . || :
+while fgrep $root/ /proc/mounts
+do fgrep $root/ /proc/mounts | while read h i j
+   do umount $i || :
    done 
 done
