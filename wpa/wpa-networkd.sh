72f8468555d2eee190211948b50794f49c88abc0
Wladimir Mutel <mwg@mwg.dp.ua>
2026-02-11 19:38:28 +0200

    reworked wpa-networkd to be activated from udev

--- wpa/wpa-networkd.sh
+++ wpa/wpa-networkd.sh
@@ -1,13 +1,16 @@
-for ifn in $(iw dev | awk '$1=="Interface"{print $2}') 
-do cf=/etc/wpa_supplicant/wpa_supplicant-$ifn.conf
-   [ -s $cf ] || {
-    ls -t /etc/wpa_supplicant/wpa_supplicant-*.conf | head -n1 | while read cf0
-    do [ -s "$cf0" ] && cp -av $cf0 $cf
-    done
-    [ -s $cf ] || { umask 022 
-     egrep -i '(update_config|ctrl_interface)=' /usr/share/doc/wpasupplicant/examples/wpa-roam.conf | sed 's/^#//' > $cf
-    }
-   }
-   systemctl is-active wpa_supplicant && systemctl stop wpa_supplicant
-   systemctl start wpa_supplicant@$ifn
-done
+ws=wpa_supplicant
+sn=$ws@$INTERFACE
+systemctl is-active $sn && return
+cd /etc/$ws
+cf=$ws-$INTERFACE.conf
+[ -s $cf ] || { ls -t $ws-*.conf | head -n1 | while read cf0 # $ws interface-specific config missing
+                                              do [ -s "$cf0" ] && cp -av $cf0 $cf # copy last-modified config for other interface
+                                              done
+                [ -s $cf ] || { umask 022 # could not find what to copy
+                                egrep -i '(update_config|ctrl_interface)=' /usr/share/doc/wpasupplicant/examples/wpa-roam.conf | sed 's/^#//' > $cf # create empty $cf from a bundled example
+                              }
+              }
+systemctl is-active $ws && { systemctl stop $ws
+                             systemctl is-enabled $ws && systemctl disable $ws # stop and disable global $ws
+                           }
+systemctl --no-block start $sn # start interface-specific $ws
